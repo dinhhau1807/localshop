b74ebda11e88fbcae51a8bb72fb500eff0aab87e
NDHau <nguyendinhhau1807@gmail.com>
2020-01-22 02:16:30 +0700

    Add project files.

---
+++ localshop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="localshop.MvcApplication" Language="C#" %>
---
+++ localshop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(localshop.Startup))]
+namespace localshop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
