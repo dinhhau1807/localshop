0b9f93850323a96fd137ae82a9d05be1cc7ce976
NDHau <nguyendinhhau1807@gmail.com>
2020-02-07 18:31:09 +0700

    Client: clean client, add new assets, _layout, split _layout

--- localshop.Web/App_Start/BundleConfig.cs
+++ localshop.Web/App_Start/BundleConfig.cs
@@ -8,30 +8,30 @@ namespace localshop
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            BundleTable.EnableOptimizations = true;
+            BundleTable.EnableOptimizations = false;
 
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            //            "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            //// Use the development version of Modernizr to develop with and learn from. Then, when you're
+            //// ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          "~/Scripts/bootstrap.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/bootstrap.css",
+            //          "~/Content/site.css"));
 
-            bundles.Add(new StyleBundle("~/cores/admin/css").Include(
-                "~/assets/admin/css/bootstrap.min.css",
-                "~/assets/admin/css/icons.min.css",
-                "~/assets/admin/css/app.min.css"));
+            //bundles.Add(new StyleBundle("~/cores/admin/css").Include(
+            //    "~/assets/admin/css/bootstrap.min.css",
+            //    "~/assets/admin/css/icons.min.css",
+            //    "~/assets/admin/css/app.min.css"));
         }
     }
 }
---
+++ localshop.Web/Controllers/NavController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace localshop.Controllers
+{
+    public class NavController : Controller
+    {
+        [ChildActionOnly]
+        public PartialViewResult Header()
+        {
+            return PartialView("_Header");
+        }
+
+        [ChildActionOnly]
+        public PartialViewResult Footer()
+        {
+            return PartialView("_Footer");
+        }
+    }
+}
